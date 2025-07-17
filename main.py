330a1de449100e15c014930af7b5ca8402dd44e2
George8805 <flyawayy01@gmail.com>
2025-07-17 18:58:32 +0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
 import time
 
-BOT_TOKEN = "7718252241:AAFUjt2e0383S6mz3kcHtfm1kXw5aYHOm5c"
+BOT_TOKEN = "7718252241:AAFujt2e083856mz3kcHtFmlkXw5aYHOm5c"
+
+def remove_webhook():
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/deleteWebhook"
+    response = requests.get(url)
+    print("Webhook dezactivat:", response.json())
 
 def get_updates():
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/getUpdates"
@@ -12,9 +17,11 @@ def extract_chat_id():
     updates = get_updates()
     if "result" in updates and len(updates["result"]) > 0:
         chat = updates["result"][-1]["message"]["chat"]
-        print(f"Chat ID-ul tău este: {chat['id']}")
+        print(f"✅ Chat ID-ul tău este: {chat['id']}")
     else:
-        print("Trimite un mesaj botului tău în Telegram și apoi rulează din nou scriptul.")
+        print("❗ Trimite un mesaj botului tău în Telegram și apoi rulează din nou scriptul.")
 
 if __name__ == "__main__":
+    remove_webhook()
+    time.sleep(1)
     extract_chat_id()
